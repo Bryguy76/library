35e7cdbb726249c042a1cf1972554f139d44f28d
Bryguy76 <bryant.bourgeois@comcast.net>
2022-12-18 19:10:02 -0700

    Start implimentation of toggle read attribute in card html

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const pagesInput = document.querySelector('#pages');
 const readInput = document.querySelector('#read');
+const toggleReadButton = document.querySelectorAll('.toggle-read');
+let books = [];
+let html = [];
+
+class Book {
+  constructor(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = !!read;
+  }
+  toggleRead() {
+    this.read = !this.read;
+  }
+}
 
 function toggleModal() {
   modalWindow.classList.toggle('hidden');
@@ -20,45 +36,48 @@ closeModalButton.addEventListener('click', function () {
   toggleModal();
 });
 
-let books = [];
-
-class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
-}
-
-let html = [];
-function updateLibraryDisplay(book) {
+function addToLibraryDisplay(book) {
   html.push(`
       <div class="book">
         <h2>${book.title}</h2>
         <h3>By: ${book.author}</h3>
         <h4>Page count: ${book.pages}</h4>
-        <h4>Read: ${book.read ? 'yes' : 'no'}</h4>
+        <h4 class="book${book.id}">Read: ${book.read ? 'yes' : 'no'}</h4>
+        <button class="toggle-read" id="${book.id}">Toggle Read</button>
       </div>
     `);
   booksSection.innerHTML += html[html.length - 1];
+
+  function updateReadDisplay(bookId) {
+    if (books[bookId - 1].read) {
+      document.querySelector(`.book${bookId}`).textContent = 'Read: yes';
+    } else {
+      document.querySelector(`.book${bookId}`).textContent = 'Read: no';
+    }
+  }
+
+  document.getElementById(`${book.id}`).addEventListener('click', function () {
+    // const bookId = books.length;
+    books[book.id - 1].toggleRead();
+    updateReadDisplay(book.id);
+  });
 }
 
 function clearInput() {
   titleInput.value = '';
   authorInput.value = '';
   pagesInput.value = '';
-  readInput.value = '';
+  readInput.value = null;
 }
-
 newBookSubmit.addEventListener('click', function () {
   event.preventDefault();
+  let id = books.length + 1;
   let title = titleInput.value;
   let author = authorInput.value;
   let pages = pagesInput.value;
   let read = readInput.value;
-  books.push(new Book(title, author, pages, read));
-  updateLibraryDisplay(books[books.length - 1]);
+  books.push(new Book(id, title, author, pages, read));
+  addToLibraryDisplay(books[books.length - 1]);
   toggleModal();
   clearInput();
 });
